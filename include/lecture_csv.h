619f9b52996a51f2c3fc7ec67f92922f05b64623
ThisALV <lel.charriere@orange.fr>
2021-11-22 18:14:38 +0100

    lecture_csv header and implementation

---
+++ include/lecture_csv.h
@@ -0,0 +1,15 @@
+#ifndef LECTURE_CSV_H
+#define LECTURE_CSV_H
+
+#include <stdio.h>
+#include <utils_sd.h>
+
+
+/// \fn Lis le fichier donne au format CSV et stocke les lignes et les mots, en-tete comprise, dans les cases de la matrice
+/// \param[in] fichier_csv Pointeur vers le fichier CSV ouvert
+/// \param[in] separateurs Liste des caracteres utilisees pour separer les mots au sein d'une meme ligne
+/// \param[out] mots Matrice qui contiendra dans chaque ligne les mots lues dans le fichier, en cas d'erreur d'allocation memoire, elems vaudra NULL
+void lire_fichier_votes(FILE* fichier_csv, char* separateurs, t_mat_char_star_dyn* mots);
+
+
+#endif // LECTURE_CSV_H
---
+++ src/lecture_csv.c
@@ -0,0 +1,57 @@
+#include <lecture_csv.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+// Selon les exemples fournis par Moodle, aucun CSV n'aura une lignes
+// de plus de 1024 colonnes
+#define LIGNE_MAX_TAILLE 1024
+
+// #define VERIFIER_REALLOC(mots, )
+
+
+void lire_fichier_votes(FILE* fichier_csv, char* separateurs, t_mat_char_star_dyn* mots) {
+    char ligne[LIGNE_MAX_TAILLE];
+
+    mots->lignes = 0;
+    mots->colonnes = 0;
+    // Vaut NULL au debut, donc le premier realloc fera un malloc
+    mots->elems = NULL;
+    // On lit ligne par ligne le fichier CSV en les stockant tour a tour
+    // dans le buffer
+    while (fgets(ligne, LIGNE_MAX_TAILLE, fichier_csv) != NULL) {
+        // On incremente le compteur de lignes et on retient la ligne
+        // courante
+        int ligne_i = mots->lignes++;
+
+        // On alloue une nouvelle case dans le tableau des lignes
+        // pour contenir le pointeur vers la memoire de la nouvelle ligne
+        mots->elems = (char***) realloc(mots->elems, mots->lignes * sizeof(char**));
+        // On met la memoire de la nouvelle ligne a NULL, comme
+        // ca au premier realloc qui devra lui donner de la memoire
+        // fera un malloc
+        mots->elems[ligne_i] = NULL;
+
+        char* mot = ligne; // On commence avec la ligne complete
+        mot = strtok(mot, separateurs); // On la decoupe avec les separateurs
+
+        // On initialise le compteur de colonnes de cette ligne
+        mots->colonnes = 0;
+        // Pour chaque mot dans la ligne
+        while (mot != NULL) {
+            // On incremente le compteur de colonne et on retient l'indice de celle
+            // sur laquelle on travaille
+            int colonne_i = mots->colonnes++;
+
+            // On alloue la memoire pour une nouvelle colonne de cette ligne
+            mots->elems[ligne_i] = (char**) realloc(mots->elems[ligne_i], mots->colonnes * sizeof(char*));
+            char** ligne_mat = mots->elems[ligne_i];
+
+            // On alloue dans cette dans colonne un tableau de caracteres suffisament grand
+            // pour contenir le prochainn mot de la ligne
+            ligne_mat[colonne_i] = (char*) malloc(strlen(mot) * sizeof(char));
+            // Et on copie le mot dans la matrice
+            strcpy(ligne_mat[colonne_i], mot);
+        }
+    }
+}
