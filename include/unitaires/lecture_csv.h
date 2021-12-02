6343995cda492bd0d2fd585f05161a04701a5beb
ThisALV <lel.charriere@orange.fr>
2021-12-02 16:26:44 +0100

    tetes_de_listes unit testing

--- include/unitaires/lecture_csv.h
+++ include/unitaires/lecture_csv.h
@@ -36,6 +36,12 @@ void premiers_de_ballot_egalites();
 void premiers_de_ballot_rangs_invalides();
 void premiers_de_ballot_tous_les_cas();
 
+// tetes_de_listes
+void tetes_de_listes_ok();
+void tetes_de_listes_aucun_electeur();
+void tetes_de_listes_nb_colonnes_invalides();
+void tetes_de_listes_rangs_invalides();
+
 /// \fn Execute les tests unitaires pour le module lecture_csv
 void tests_unitaires_lecture_csv();
 
