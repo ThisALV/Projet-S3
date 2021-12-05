122a6b40a2cc082131a26165cc8674843abc42c1
ThisALV <lel.charriere@orange.fr>
2021-12-05 23:47:35 +0100

    vainqueur_uninominale implementation

--- include/utils_scrutins.h
+++ include/utils_scrutins.h
@@ -16,5 +16,30 @@
 /// un des ages est inconnu
 int departager_candidats(t_candidats candidats);
 
+/// \fn Compte le nb de voix pour chaque candidats en utilisant les candidats preferes
+/// de chaque ballot de votes
+/// \param[out] voix Decompte des voix pour chaque candidat, un indice du tab est un ID de candidat
+/// \param[in] candidats Necessaire pour connaitre les infos qui serviront a
+/// departager les candidats en cas d'egalite
+/// \param[in] tetes_de_listes Tableau des candidats preferes de chaque ballots
+/// \param[in] nb_ballots Nb d'elements dans `tetes_de_listes`
+void compter_voix_ballots(t_tab_int_dyn* voix, t_candidats candidats, t_tab_int_dyn* tetes_de_listes, int nb_ballots);
+
+/// \fn Comparer les voix obtenues par les candidats pour determiner et departager
+/// si necessaire qui est le vainqueur
+/// \param[in] voix Nb de voix obtenu pour chaque candidat, un indice du tab est un ID de candidat
+/// \param[in] candidats Necessaire pour connaitre les infos qui serviront a
+/// departager les candidats en cas d'egalite
+/// \return L'ID du candidat qui a ete calcule comme etant vainqueur
+int comparer_voix_ballots(t_tab_int_dyn voix, t_candidats candidats);
+
+/// \fn Calcule le vainqueur d'un tour de vote selon la methode uninominale
+/// \param[in] candidats Necessaire pour connaitre le nb de candidats et pour acceder a
+/// leurs noms/ages qui serviront a les departager en cas d'egalite
+/// \param[in] tetes_de_listes Tableau des candidats preferes de chaque ballots
+/// \param[in] nb_ballots Nombre de tableaux des preferes dans `tetes_de_listes`
+/// \return L'ID du candidat designe vainqueur selon cette methode
+int vainqueur_uninominale(t_candidats candidats, t_tab_int_dyn* tetes_de_listes, int nb_ballots);
+
 
 #endif // UTILS_SCRUTINS_H
--- src/utils_scrutins.c
+++ src/utils_scrutins.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <erreur.h>
 
 // Rang d'anciennetee pour les candidats dont on ne connait pas l'age
 #define RANG_AGE_NON_TROUVE -1
@@ -86,4 +87,85 @@ int departager_candidats(t_candidats candidats) {
 
     // Grace a l'indice obtenu, on accede a l'ID du candidat vainqueur
     return candidats.elems[vainqueur_i].id;
+}
+
+// Pour chaque ID dans ids, retrouve les infos du candidat ayant cet ID dans
+// candidats, et l'ajoute dans le tableau `filtres`
+// Il est important que `candidats` soit une BDD de candidats conforme au
+// programme, cad que l'indice ce chaque candidat soit == a son ID, donc dans l'ordre
+static void recuperer_infos_pour(t_candidats* filtres, t_candidats candidats, t_tab_int_dyn ids) {
+    filtres->elems = NULL; // 1er realloc = malloc, puis realloc quand on veut agrandir le tableau
+    filtres->nb = 0;
+
+    for (int id_i = 0; id_i < ids.taille; id_i++) {
+        int id_candidat = ids.elems[id_i]; // ID de candidat selectionne
+
+        // On ajoute une place dans le tableau
+        filtres->nb++;
+        filtres->elems = (t_candidat*) realloc(
+            filtres->elems, filtres->nb * sizeof(t_candidat));
+        verifier_alloc(filtres->elems, "Allocation infos candidats");
+
+        // On assigne le candidat selectionne a cette place
+        filtres->elems[filtres->nb - 1] = candidats.elems[id_candidat];
+    }
+}
+
+void compter_voix_ballots(t_tab_int_dyn* voix, t_candidats candidats, t_tab_int_dyn* tetes_de_listes, int nb_ballots) {
+    for (int ballot_i = 0; ballot_i < nb_ballots; ballot_i++) {
+        t_tab_int_dyn preferes = tetes_de_listes[ballot_i];
+        int vainqueur_ballot_id;
+
+        // S'il y a une egalite dans ce ballot de votes
+        if (preferes.taille != 1) { // Alors on departage
+            t_candidats infos_preferes;
+            recuperer_infos_pour(&infos_preferes, candidats, preferes);
+
+            vainqueur_ballot_id = departager_candidats(infos_preferes);
+
+            detruire_t_candidats_dyn(&infos_preferes);
+        } else { // Sinon on recupere l'ID de l'unique vainqueur du ballot
+            vainqueur_ballot_id = preferes.elems[0];
+        }
+
+        // On a determine l'unique candidat pour qui vote l'electeur ayant
+        // fait ce ballot de votes, on peut le compter comme une voix
+        voix->elems[vainqueur_ballot_id]++;
+    }
+}
+
+int comparer_voix_ballots(t_tab_int_dyn voix, t_candidats candidats) {
+    // On recupere l'ID du candidat ayant obtenu le maximum de voix
+    int vainqueur_id = 0;
+    int nb_voix_vainqueur = voix.elems[vainqueur_id];
+    for (int candidat_id = 1; candidat_id < candidats.nb; candidat_id++) {
+        int nb_voix_courant = voix.elems[candidat_id];
+
+        // S'il y a une egalite entre le meilleur candidat et le candiat courant
+        if (nb_voix_courant == nb_voix_vainqueur) {
+            // On creer notre situation d'egalitee...
+            t_candidats egalite;
+            creer_t_candidats_dyn(&egalite, 2);
+            egalite.elems[0] = candidats.elems[vainqueur_id];
+            egalite.elems[1] = candidats.elems[candidat_id];
+
+            // ...pour l'analyser afin de determiner qui est le candidat prioritaire
+            vainqueur_id = departager_candidats(egalite);
+
+            // On n'oublie de detruire notre situation d'egalitee
+            detruire_t_candidats_dyn(&egalite);
+        }
+    }
+
+    return vainqueur_id;
+}
+
+int vainqueur_uninominale(t_candidats candidats, t_tab_int_dyn* tetes_de_listes, int nb_ballots) {
+    // Dans ce tableau, chaque indice est l'ID d'un candidat auquel un associe
+    // un entier qui est son nb de voix
+    t_tab_int_dyn voix;
+    creer_t_tab_int_dyn(&voix, candidats.nb); // Toutes les voix commencent a 0
+
+    compter_voix_ballots(&voix, candidats, tetes_de_listes, nb_ballots);
+    return comparer_voix_ballots(voix, candidats);
 }
