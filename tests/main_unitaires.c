85218620cc47c549fdc6dbc04e2e42433d266e8a
ThisALV <lel.charriere@orange.fr>
2021-12-21 13:07:11 +0100

    Fix: Initializes logging output at main()

--- tests/main_unitaires.c
+++ tests/main_unitaires.c
@@ -1,3 +1,4 @@
+#include <log.h>
 #include <unitaires/utils_sd.h>
 #include <unitaires/lecture_csv.h>
 #include <unitaires/utils_scrutins.h>
@@ -12,6 +13,8 @@ void test_unitaire_module(char* nom_module, void (*script_tests)()) {
 }
 
 int main() {
+    sortie_logging_par_defaut();
+
     test_unitaire_module("utils_sd", tests_unitaires_utils_sd);
     test_unitaire_module("lecture_csv", tests_unitaires_lecture_csv);
     test_unitaire_module("utils_scrutin", tests_unitaires_utils_scrutin);
