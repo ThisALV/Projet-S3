6f38f506020e2c995093f4231785d32f830d66bd
ThisALV <lel.charriere@orange.fr>
2022-01-06 00:11:18 +0100

    Imports sha moodle provided files

---
+++ include/utils_verifier_mon_vote.h
@@ -0,0 +1,13 @@
+/// \file Fonctions utilisees par le programme verifier_mon_vote, mises dans un module
+/// a part afin d'etre testees
+/// \author CHARRIERE Lelio
+/// \date 05/01/2022
+
+#ifndef UTILS_VERIFIER_MON_VOTE_H
+#define UTILS_VERIFIER_MON_VOTE_H
+
+
+
+
+
+#endif // UTILS_VERIFIER_MON_VOTE_H
