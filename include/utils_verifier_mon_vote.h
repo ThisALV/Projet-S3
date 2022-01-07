b5a492e8823a66ed81cd580be038931d93d3f434
ThisALV <lel.charriere@orange.fr>
2022-01-07 23:45:31 +0100

    chercher_electeur implementation

--- include/utils_verifier_mon_vote.h
+++ include/utils_verifier_mon_vote.h
@@ -46,5 +46,18 @@ void ecrire_fichier_votes(FILE* fichier_csv, char separateur, t_mat_char_star_dy
 /// \return `true` si l'operation a entierement reussie, `false` sinon
 bool chiffrer_ballots_votes(t_mat_char_star_dyn mots_csv, char** cles_privees, FILE* sortie_cles);
 
+/// \fn Cherche dans la matrice CSV les noms et scores des candidats pour lequel
+/// l'electeur avec le nom et la cle privee specifies a vote
+/// \param[in] mots_csv Liste de ballots lue au format CSV
+/// \param[in] nom_electeur Nom de l'electeur utilise pour former son hash
+/// \param[in] cle_privee Cle de l'electeur utilisee pour former son hash
+/// \param[out] noms_candidats Noms des candidats presents dans ce scrutin.
+/// \param[out] scores_candidats Scores, ecrits tels quels dans le CSV, que
+/// l'electeur a donne a chaque candidat de `noms_candidats`. En mode erreur
+/// si l'electeur n'a pas ete trouve.
+/// \return `true` si le fichier CSV d'entree est bien forme, `false` sinon
+bool chercher_electeur(t_mat_char_star_dyn mots_csv, char* nom_electeur, char* cle_privee,
+                       t_tab_char_star_dyn* noms_candidats, t_tab_char_star_dyn* scores_candidats);
+
 
 #endif // UTILS_VERIFIER_MON_VOTE_H
--- src/utils_verifier_mon_vote.c
+++ src/utils_verifier_mon_vote.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sha256_utils.h>
+#include <lecture_csv.h>
 
 // Est appelee pour verifier si une operation d'ecriture s'est terminee
 // avec succes, et lever une erreur fatale si ce n'est pas le cas
@@ -130,4 +131,90 @@ bool chiffrer_ballots_votes(t_mat_char_star_dyn mots_csv, char** cles_privees, F
     }
 
     return true; // L'operation s'est fini sans erreur
+}
+
+// Si le hash de l'electeur courant est celui recherche, alors on affecte les scores
+// donnes par cet electeur aux differents candidats et on retourne `true`.
+// Sinon, on retourne `false`.
+static bool analyser_ballot_electeur(
+    char** ligne_electeur, int nb_colonnes, char* hash_recherche,
+    t_tab_char_star_dyn* scores_candidats)
+{
+    char* hash_courant = ligne_electeur[NOM_ELECTEUR_COLONNE_I];
+    if (strcmp(hash_recherche, hash_courant) != 0)
+        return false; // On passe cet electeur, ce n'est pas celui recherche
+
+    // Pour chaque colonne contenant un score de candidats, ces colonnes se
+    // situant juste apres celle contenant le hash/nom de l'electeur
+    for (int col_i = NOM_ELECTEUR_COLONNE_I + 1; col_i < nb_colonnes; col_i++) {
+        char* score = allouer_copie_char_star(ligne_electeur[col_i], "Copie score ballot");
+
+        // On calcul l'index du score a partir du nombre de colonnes prefixes
+        // et de l'index de sa colonne dans la matrice de mots CSV
+        int score_i = col_i - 4;
+        scores_candidats->elems[score_i] = score;
+    }
+
+    // On a effectue notre traitement sur l'electeur recherche
+    return true;
+}
+
+bool chercher_electeur(
+    t_mat_char_star_dyn mots_csv, char* nom_electeur, char* cle_privee,
+    t_tab_char_star_dyn* noms_candidats, t_tab_char_star_dyn* scores_candidats)
+{
+    int nb_colonnes = mots_csv.colonnes;
+
+    // On doit avoir suffisamment de colonnes par le hash de l'electeur et 1
+    // candidat
+    if (nb_colonnes < 5)
+        return false; // Sinon l'operation est impossible
+
+    // On forme la BDD des candidats a partir de la liste de ballots pour avoir
+    // acces a leur nom
+    t_candidats bdd_candidats;
+    obtenir_candidats_ballots(mots_csv, &bdd_candidats);
+
+    // On affecte les noms de tous les candidats
+    int nb_candidats = bdd_candidats.nb;
+    creer_t_tab_char_star_dyn(noms_candidats, nb_candidats);
+    for (int candidat_id = 0; candidat_id < nb_candidats; candidat_id++) {
+        char* nom = allouer_copie_char_star(
+            bdd_candidats.elems[candidat_id].nom, "Copie nom candidat");
+
+        noms_candidats->elems[candidat_id] = nom;
+    }
+
+    // On aura autant de colonnes de score que de candidats
+    creer_t_tab_char_star_dyn(scores_candidats, nb_candidats);
+
+    // C'est ce hash que l'on cherchera dans la colonne electeur des mots CSV
+    char hash_recherche[TAILLE_HASH_ELECTEUR + 1];
+    hash_electeur(nom_electeur, cle_privee, hash_recherche);
+    
+    // Au debut, on n'a pas encore trouver l'electeur
+    bool electeur_trouve = false;
+    // Sachant que la 1ere ligne est l'en-tete, donc pas un ballot de votes
+    int nb_electeurs = mots_csv.lignes - 1;
+    // Pour chaque electeur, tant qu'on a pas trouve celui qu'on recherche
+    for (int electeur_i = 0; !electeur_trouve && electeur_i < nb_electeurs; electeur_i++) {
+        int ligne_i = electeur_i + 1;
+        char** ligne_courante = mots_csv.elems[ligne_i];
+
+        // Si l'analyse a trouve l'electeur recherche, alors on peut arreter la
+        electeur_trouve = analyser_ballot_electeur(
+            ligne_courante, nb_colonnes, hash_recherche, scores_candidats);
+    }
+
+    // On libere les ressources utilisees par l'operation
+    detruire_t_candidats_dyn(&bdd_candidats);
+
+    // Si on a pas trouve l'electeur, alors on le signale en passant les scores
+    // en mode erreur
+    if (!electeur_trouve)
+        mettre_t_tab_char_star_dyn_erreur(scores_candidats);
+
+    // Peu importe si on a trouve l'electeur ou non, l'operation s'est terminee
+    // normalement
+    return true;
 }
