40cb08678f9939be528e0c0921b533232ee0658c
ThisALV <lel.charriere@orange.fr>
2022-01-08 03:35:03 +0100

    chercher_electeur unit testing

--- include/unitaires/utils_verifier_mon_vote.h
+++ include/unitaires/utils_verifier_mon_vote.h
@@ -6,6 +6,11 @@
 void hash_electeur_nom_vide();
 void hash_electeur_nom_electeur();
 
+// chercher_electeur
+void chercher_electeur_nb_colonnes_invalide();
+void chercher_electeur_electeur_non_trouve();
+void chercher_electeur_electeur_trouve();
+
 /// \fn Execute les tests unitaires pour le module utils_verifier_mon_vote
 void tests_unitaires_utils_verifier_mon_vote();
 
