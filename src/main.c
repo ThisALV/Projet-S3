9b8e93bc91c5974b4efa4fe0e907d93280d2da3d
ThisALV <lel.charriere@orange.fr>
2022-01-09 17:16:58 +0100

    Refactor: Each method type in separate function

--- src/main.c
+++ src/main.c
@@ -37,6 +37,104 @@ void afficher_scrutin(char* nom_methode, t_candidats candidats,
 }
 
 
+// Execute toutes les methodes de scrutin sur la matrice CSV donnee
+void executer_tous_scrutins(t_mat_char_star_dyn mots_csv, t_candidats bdd_candidats) {
+    // On aura besoin des 2 structures de donnees, puisque toutes les methodes
+    // seront executees
+    t_mat_int_dyn mat_duels;
+    t_tab_int_dyn* vainqueurs_ballots;
+    int nb_ballots = mots_csv.lignes - 1;
+
+    // On construit ces 2 SDD depuis la liste de ballots CSV
+    creer_mat_duels(mots_csv, &mat_duels);
+    vainqueurs_ballots = tetes_de_listes(mots_csv);
+
+    int vainqueur_id;
+    // On execute toutes les methodes de scrutin, sachant qu'on connait le nb
+    // de votants puisqu'on a forcement une liste de ballots pour les methodes
+    // uninominales
+
+    vainqueur_id = uninominale_un_tour(bdd_candidats, vainqueurs_ballots, nb_ballots);
+    afficher_scrutin("uninomianle a un tour", bdd_candidats, nb_ballots, vainqueur_id, 0);
+
+    vainqueur_id = condorcet_minimax(mat_duels, bdd_candidats);
+    afficher_scrutin("Condorcet minimax", bdd_candidats, nb_ballots, vainqueur_id, 0);
+
+    vainqueur_id = condorcet_schulze(mat_duels, bdd_candidats);
+    afficher_scrutin("Condorcet Schulze", bdd_candidats, nb_ballots, vainqueur_id, 0);
+
+    // On n'oublie pas de liberer les ressources des sdd utilisees par les methodes
+    detruire_t_mat_int_dyn(&mat_duels);
+    detruire_vainqueurs_ballots(vainqueurs_ballots, nb_ballots);
+}
+
+// Execute une des methodes uninominales
+void executer_scrutin_uninominale(t_mat_char_star_dyn mots_csv, t_candidats bdd_candidats, int methode_scrutin) {
+    // Les methodes uninominales ont besoin des premiers de chaque ballots
+    t_tab_int_dyn* vainqueurs_ballots = tetes_de_listes(mots_csv);
+    // On verifie qu'il n'y ait pas eu d'erreur a la lecture des ballots CSV
+    if (vainqueurs_ballots == NULL)
+        erreur_fatale(2, "Lecture des ballots CSV");
+    // Il y a autant de ballots que de ligne -1 puisqu'il faut ignorer l'en-tete
+    int nb_ballots = mots_csv.lignes - 1;
+
+    int vainqueur_id;
+    char* nom_methode;
+    // On execute la methode uninominale choisie
+    if (methode_scrutin == METHODE_UNINOMINALE_UN_TOUR) {
+        vainqueur_id = uninominale_un_tour(bdd_candidats, vainqueurs_ballots, nb_ballots);
+        nom_methode = "uninominale a un tour";
+    } else {
+        erreur_fatale(3, "Uninominale deux tours pas encore implementee");
+
+        // TODO: Implementer uninominale deux tours
+        vainqueur_id = -1;
+        nom_methode = "uninomnale a deux tours";
+    }
+
+    // Il y a autant de ballots que de votants
+    afficher_scrutin(nom_methode, bdd_candidats, nb_ballots, vainqueur_id, 0);
+
+    // On n'oublie pas de liberer les tableaux des vainqueurs de chaque ballot
+    detruire_vainqueurs_ballots(vainqueurs_ballots, nb_ballots);
+}
+
+// Execute une methode de Condorcet
+void executer_scrutin_condorcet(t_mat_char_star_dyn mots_csv, t_candidats bdd_candidats, int methode_scrutin) {
+    // Les autres methodes ont toutes besoin d'une matrice de duels
+    t_mat_int_dyn mat_duels;
+    // On interprete la matrice de duels depuis une matrice directement en CSV,
+    // ou depuis une liste de ballots en fonction du format d'entree
+    int nb_votants; // On essaie aussi de lire dans le fichier le nb de votants
+    if (methode_scrutin == ENTREE_MATRICE_DUELS) {
+        convertir_mat_duels(mots_csv, &mat_duels);
+        // Impossible de connaitre le nb de votants dans une matrice de duels
+        nb_votants = 0;
+    } else {
+        creer_mat_duels(mots_csv, &mat_duels);
+        nb_votants = mots_csv.lignes - 1; // Autant de votants que de ballots
+    }
+
+    int vainqueur_id;
+    char* nom_methode;
+    // On execute la methode de scrutin choisie, qui sera condorcet minimax ou Schulze
+    if (methode_scrutin == METHODE_CONDORCET_MINIMAX) {
+        vainqueur_id = condorcet_minimax(mat_duels, bdd_candidats);
+        nom_methode = "Condorcet minimax";
+    } else {
+        vainqueur_id = condorcet_schulze(mat_duels, bdd_candidats);
+        nom_methode = "Condorcet Schulze";
+    }
+
+    // Il n'y a pas de score final en % puisqu'il ne s'agit pas d'une methode
+    // uninominale
+    afficher_scrutin(nom_methode, bdd_candidats, nb_votants, vainqueur_id, 0);
+
+    // On n'oublie pas de detruire la mat de duels utilisee pour la methode
+    detruire_t_mat_int_dyn(&mat_duels);
+}
+
+
 int main(int argc, char** argv) {
     // Initialisation du module de logging
     sortie_logging_par_defaut();
@@ -80,95 +178,12 @@ int main(int argc, char** argv) {
 
     // Execution de la methode choisie et interpretation du fichier CSV en fonction
     // (ou des methodes si on choisit d'executer toutes les methodes)
-    if (params_scrutin.option_methode == METHODE_TOUTES) {
-        // On aura besoin des 2 structures de donnees, puisque toutes les methodes
-        // seront executees
-        t_mat_int_dyn mat_duels;
-        t_tab_int_dyn* vainqueurs_ballots;
-        int nb_ballots = mots_csv.lignes - 1;
-
-        // On construit ces 2 SDD depuis la liste de ballots CSV
-        creer_mat_duels(mots_csv, &mat_duels);
-        vainqueurs_ballots = tetes_de_listes(mots_csv);
-
-        int vainqueur_id;
-        // On execute toutes les methodes de scrutin, sachant qu'on connait le nb
-        // de votants puisqu'on a forcement une liste de ballots pour les methodes
-        // uninominales
-
-        vainqueur_id = uninominale_un_tour(bdd_candidats, vainqueurs_ballots, nb_ballots);
-        afficher_scrutin("uninomianle a un tour", bdd_candidats, nb_ballots, vainqueur_id, 0);
-
-        vainqueur_id = condorcet_minimax(mat_duels, bdd_candidats);
-        afficher_scrutin("Condorcet minimax", bdd_candidats, nb_ballots, vainqueur_id, 0);
-
-        vainqueur_id = condorcet_schulze(mat_duels, bdd_candidats);
-        afficher_scrutin("Condorcet Schulze", bdd_candidats, nb_ballots, vainqueur_id, 0);
-
-        // On n'oublie pas de liberer les ressources des sdd utilisees par les methodes
-        detruire_t_mat_int_dyn(&mat_duels);
-        detruire_vainqueurs_ballots(vainqueurs_ballots, nb_ballots);
-    } else if (methode_uninominale) {
-        // Les methodes uninominales ont besoin des premiers de chaque ballots
-        t_tab_int_dyn* vainqueurs_ballots = tetes_de_listes(mots_csv);
-        // On verifie qu'il n'y ait pas eu d'erreur a la lecture des ballots CSV
-        if (vainqueurs_ballots == NULL)
-            erreur_fatale(2, "Lecture des ballots CSV");
-        // Il y a autant de ballots que de ligne -1 puisqu'il faut ignorer l'en-tete
-        int nb_ballots = mots_csv.lignes - 1;
-
-        int vainqueur_id;
-        char* nom_methode;
-        // On execute la methode uninominale choisie
-        if (params_scrutin.option_methode == METHODE_UNINOMINALE_UN_TOUR) {
-            vainqueur_id = uninominale_un_tour(bdd_candidats, vainqueurs_ballots, nb_ballots);
-            nom_methode = "uninominale a un tour";
-        } else {
-            erreur_fatale(3, "Uninominale deux tours pas encore implementee");
-
-            // TODO: Implementer uninominale deux tours
-            vainqueur_id = -1;
-            nom_methode = "uninomnale a deux tours";
-        }
-
-        // Il y a autant de ballots que de votants
-        afficher_scrutin(nom_methode, bdd_candidats, nb_ballots, vainqueur_id, 0);
-
-        // On n'oublie pas de liberer les tableaux des vainqueurs de chaque ballot
-        detruire_vainqueurs_ballots(vainqueurs_ballots, nb_ballots);
-    } else {
-        // Les autres methodes ont toutes besoin d'une matrice de duels
-        t_mat_int_dyn mat_duels;
-        // On interprete la matrice de duels depuis une matrice directement en CSV,
-        // ou depuis une liste de ballots en fonction du format d'entree
-        int nb_votants; // On essaie aussi de lire dans le fichier le nb de votants
-        if (params_scrutin.format_entree == ENTREE_MATRICE_DUELS) {
-            convertir_mat_duels(mots_csv, &mat_duels);
-            // Impossible de connaitre le nb de votants dans une matrice de duels
-            nb_votants = 0;
-        } else {
-            creer_mat_duels(mots_csv, &mat_duels);
-            nb_votants = mots_csv.lignes - 1; // Autant de votants que de ballots
-        }
-
-        int vainqueur_id;
-        char* nom_methode;
-        // On execute la methode de scrutin choisie, qui sera condorcet minimax ou Schulze
-        if (params_scrutin.option_methode == METHODE_CONDORCET_MINIMAX) {
-            vainqueur_id = condorcet_minimax(mat_duels, bdd_candidats);
-            nom_methode = "Condorcet minimax";
-        } else {
-            vainqueur_id = condorcet_schulze(mat_duels, bdd_candidats);
-            nom_methode = "Condorcet Schulze";
-        }
-        
-        // Il n'y a pas de score final en % puisqu'il ne s'agit pas d'une methode
-        // uninominale
-        afficher_scrutin(nom_methode, bdd_candidats, nb_votants, vainqueur_id, 0);
-
-        // On n'oublie pas de detruire la mat de duels utilisee pour la methode
-        detruire_t_mat_int_dyn(&mat_duels);
-    }
+    if (params_scrutin.option_methode == METHODE_TOUTES)
+        executer_tous_scrutins(mots_csv, bdd_candidats);
+    else if (methode_uninominale)
+        executer_scrutin_uninominale(mots_csv, bdd_candidats, params_scrutin.option_methode);
+    else
+        executer_scrutin_condorcet(mots_csv, bdd_candidats, params_scrutin.option_methode);
 
     // On desalloue toutes les ressources allouees pour le programme
     detruire_t_mat_char_star_dyn(&mots_csv);
